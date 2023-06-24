3da39fa7d785f26465a2cecce13da8111b558f4d
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-06-24 19:04:01 -0300

    Remove large function call

--- src/components/dashboard-card.tsx
+++ src/components/dashboard-card.tsx
@@ -4,10 +4,6 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { ImageResponse } from 'next/server';
-import { GET } from '../app/api/og/route';
-
-import { headers } from "next/headers";
 
 export async function DashboardCard({ htmlString, name }: { htmlString: string; name: string }) {
   // const [imageUrl, setImageUrl] = useState<string>();
@@ -25,22 +21,6 @@ export async function DashboardCard({ htmlString, name }: { htmlString: string; 
   //   getImage();
   // }, []);
 
-  const host = headers().get("host");
-  console.log(host)
-  // const req = new Request("/?name=abc123")
-  // const res = await GET(req);
-  // console.log(res)
-
-  const res = await fetch(`http://${host}/api/og/?name=${name}&element=${htmlString}}`);
-  const imgBlob = await res.blob();
-  const imgSrc = URL.createObjectURL(imgBlob);
-
-  console.log(imgSrc)
-
-  const element = (
-    <div style={{ backgroundColor: 'red', width: '200px', height: '200px' }}>Hello, World!</div>
-  );
-
   return (
     <Card className="max-w-md">
       <CardHeader>
@@ -50,7 +30,7 @@ export async function DashboardCard({ htmlString, name }: { htmlString: string; 
         {/* {imageUrl && (
           <Image src={imageUrl} alt="Vercel" width={232} height={200} />
         )} */}
-        {imgSrc && <Image src={imgSrc} alt="Vercel" width={232} height={200} />}
+        {/* {imgSrc && <Image src={imgSrc} alt="Vercel" width={232} height={200} />} */}
       </CardContent>
     </Card>
   );
