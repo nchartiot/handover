88fffd053507c317791381cd8bd498c94fbaf58b
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-07-03 19:20:59 -0300

    Add Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import { Inter } from 'next/font/google';
 
 import { SiteHeader } from '@/components/site-header';
 import { ThemeProvider } from '@/components/theme-provider';
@@ -9,11 +10,17 @@ export const metadata = {
   title: 'Handover',
 };
 
+const interFont = Inter({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <main className="min-w-screen flex flex-col">
+        <main className={`min-w-screen flex flex-col ${interFont.className}`}>
           <ThemeProvider attribute="class" defaultTheme="light">
             <SiteHeader />
             {children}
