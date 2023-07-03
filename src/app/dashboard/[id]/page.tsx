fa2b4e63c6f333be2da7fb1cf915528e8f1f59bc
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-07-03 18:51:26 -0300

    Update metadata

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,19 +1,58 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { ChevronLeft } from 'lucide-react';
+import { Metadata } from 'next';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
+import { Suspense } from 'react';
 
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 import { Database } from '@/types/supabase';
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const screenId = params.id;
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const { data: screen } = await supabase.from('screens').select('*').eq('id', screenId).single();
+
+  return {
+    title: `${screen?.name} - Handover`,
+  };
+}
 
 export default async function ScreenPage({ params }: { params: { id: number } }) {
   const screenId = params.id || 0;
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  const { data: screen } = await supabase.from('screens').select('*').eq('id', screenId);
+  const { data: screen } = await supabase.from('screens').select('*').eq('id', screenId).single();
+  console.log(screen?.html_file);
 
   return (
-    <div className="px-14 py-4">
-      <div className="flex items-center justify-between">
-        <pre>{JSON.stringify(screen)}</pre>
+    <div className="px-14 py-8">
+      <div className="flex h-10 items-center gap-4">
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/dashboard">
+            <ChevronLeft className="h-4 w-4" />
+          </Link>
+        </Button>
+        <Suspense fallback={<Skeleton />}>
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{screen?.name}</h3>
+        </Suspense>
       </div>
+      <hr className="my-8" />
+
+      <Suspense fallback={<Skeleton />}>
+        <div className="flex items-center justify-between">
+          <div
+            className="h-96 w-96"
+            dangerouslySetInnerHTML={{ __html: screen?.html_file as string }}
+          ></div>
+        </div>
+      </Suspense>
     </div>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,29 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Metadata } from 'next';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
 
 import { DashboardCard } from '@/components/dashboard-card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { UploadScreenDialog } from '@/components/upload-screen-dialog';
 import { Database } from '@/types/supabase';
 
 export const revalidate = 0;
+
+export const metadata: Metadata = {
+  title: 'Dashboard - Handover',
+};
+
+function LoadingDashboard() {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <Skeleton className="h-52 max-w-md" key={i} />
+      ))}
+    </div>
+  );
+}
 
 export default async function DashboardPage() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -19,21 +36,20 @@ export default async function DashboardPage() {
   if (!user) redirect('/');
 
   return (
-    <div className="px-14 py-4">
-      <div className="flex items-center justify-between">
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Welcome, {user.email}</h3>
+    <div className="px-14 py-8">
+      <div className="flex h-10 items-center justify-between">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Screen Dashboard!</h3>
         <UploadScreenDialog />
       </div>
-      <hr className="mb-12 mt-10" />
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {screens?.map((screen) => (
-          <DashboardCard
-            key={screen.id}
-            name={screen.name}
-            id={screen.id}
-          />
-        ))}
-      </div>
+      <hr className="my-8" />
+
+      <Suspense fallback={<LoadingDashboard />}>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {screens?.map((screen) => (
+            <DashboardCard key={screen.id} name={screen.name} id={screen.id} />
+          ))}
+        </div>
+      </Suspense>
     </div>
   );
 }
