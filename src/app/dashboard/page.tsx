952ee1e61813344fe3c0aeb6da82f5d22455ef86
Juliano <jh.bortoli@gmail.com>
2023-07-04 14:34:20 -0300

    Added query to latest_screens view

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -28,7 +28,7 @@ function LoadingDashboard() {
 export default async function DashboardPage() {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  const { data: screens } = await supabase.from('screens').select('name, id').order("version", {ascending: false, nullsFirst: true}); // Need to add isActive column in Supabase, then we can filter only the active screens more  easily
+  const { data: screens } = await supabase.from('latest_screens').select('name, id').order("version", {ascending: false, nullsFirst: true}); // Need to add isActive column in Supabase, then we can filter only the active screens more  easily
   
   const {
     data: { user },
