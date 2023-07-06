1e052f11437cd30c8767159d33c70e3b205fdaf0
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-07-06 18:38:11 -0300

    Update supabase types

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -9,36 +9,108 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      comments: {
+        Row: {
+          comment: string
+          created_at: string
+          id: string
+          screen_id: string
+          user_id: string
+        }
+        Insert: {
+          comment: string
+          created_at?: string
+          id: string
+          screen_id: string
+          user_id: string
+        }
+        Update: {
+          comment?: string
+          created_at?: string
+          id?: string
+          screen_id?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "comments_screen_id_fkey"
+            columns: ["screen_id"]
+            referencedRelation: "screens"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "comments_screen_id_fkey"
+            columns: ["screen_id"]
+            referencedRelation: "latest_screens"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "comments_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       screens: {
         Row: {
           changes: string
-          created_at: string | null
+          created_at: string
           html_file: string
-          id: number
+          id: string
+          is_svg: boolean
           name: string
           version: number
         }
         Insert: {
           changes: string
-          created_at?: string | null
+          created_at?: string
           html_file: string
-          id?: number
+          id?: string
+          is_svg: boolean
           name: string
           version: number
         }
         Update: {
           changes?: string
-          created_at?: string | null
+          created_at?: string
           html_file?: string
-          id?: number
+          id?: string
+          is_svg?: boolean
           name?: string
           version?: number
         }
         Relationships: []
       }
+      users: {
+        Row: {
+          email: string
+          id: string
+        }
+        Insert: {
+          email: string
+          id: string
+        }
+        Update: {
+          email?: string
+          id?: string
+        }
+        Relationships: []
+      }
     }
     Views: {
-      [_ in never]: never
+      latest_screens: {
+        Row: {
+          changes: string | null
+          created_at: string | null
+          html_file: string | null
+          id: string | null
+          is_svg: boolean | null
+          name: string | null
+          version: number | null
+        }
+        Relationships: []
+      }
     }
     Functions: {
       [_ in never]: never
