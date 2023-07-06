f102994cbf90a47fabc2c937a6b6067c9d5aa56d
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-07-06 18:07:07 -0300

    Update homepage to include link to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 import { HeroImage } from '@/components/hero-image';
 import { LoginDialog } from '@/components/login-dialog';
+import { Button } from '@/components/ui/button';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
-  if (user) {
-    redirect('/dashboard');
-  }
 
   return (
     <div className="bg-dotted-spacing-4 bg-dotted-gray-200 dark:bg-dotted-gray-900">
@@ -37,12 +34,19 @@ export default async function Home() {
                 Data to enrich your online business
               </h1>
               <p className="mt-6 text-lg leading-8">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+                commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-                <LoginDialog />
+                {user ? (
+                  <Button asChild>
+                    <Link href="/dashboard" className="ml-8!">
+                      Go to dashboard
+                    </Link>
+                  </Button>
+                ) : (
+                  <LoginDialog />
+                )}
               </div>
             </div>
             <div className="mt-16 flow-root sm:mt-24">
@@ -54,59 +58,3 @@ export default async function Home() {
     </div>
   );
 }
-
-// import { cookies } from 'next/headers';
-// import Balancer from 'react-wrap-balancer';
-
-// import { Icons } from '@/components/icons';
-// import { LoginDialog } from '@/components/login-dialog';
-// import { SiteFooter } from '@/components/site-footer';
-
-// export default async function Home() {
-//   const supabase = createServerComponentClient({ cookies });
-//   const {
-//     data: { user },
-//   } = await supabase.auth.getUser();
-
-//   return (
-//     <section className="flex h-full flex-1 flex-col">
-//       <div className="relative isolate h-full flex-1 overflow-hidden">
-//         <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
-//           <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-//             <h1 className="mt-10 text-4xl font-bold tracking-tight sm:text-6xl">
-//               Endless art, <br /> one Frame
-//             </h1>
-//             <p className="mt-6 text-lg leading-8">
-//               <Balancer>
-//                 Bring an infinite gallery to your desk and easily update your
-//                 curated collection of arts, quotes and photos. The Frame is
-//                 designed for the modern minimalist.
-//               </Balancer>
-//             </p>
-//             <div className="mt-10 flex items-center gap-x-6">
-//               {user ? <p>Welcome {user.email}</p> : <LoginDialog />}
-//               <a
-//                 href="#"
-//                 className="flex items-center gap-1 text-sm font-semibold leading-6"
-//               >
-//                 Learn more <Icons.arrowRight className="h-3 w-3" />
-//               </a>
-//             </div>
-//           </div>
-//           <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-//             <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-//               <img
-//                 src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-//                 alt="App screenshot"
-//                 width={2432}
-//                 height={1442}
-//                 className="w-[76rem] rounded-md  shadow-2xl ring-1 ring-white/10"
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <SiteFooter />
-//     </section>
-//   );
-// }
