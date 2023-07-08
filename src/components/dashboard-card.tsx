f31b030a2461ea37a5cdc2a952ca6d07c80d9a12
jbortoli <jbortoli@DESKTOP-AUSG0DG>
2023-07-08 15:40:14 -0300

    Adding preview images

--- src/components/dashboard-card.tsx
+++ src/components/dashboard-card.tsx
@@ -3,26 +3,31 @@
 import { useRouter } from 'next/navigation';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import parse from 'html-react-parser';
 
 type DashboardCardProps = {
   name: string;
   id: string;
+  htmlFile?: string;
 };
 
-export async function DashboardCard({ name, id }: DashboardCardProps) {
+export async function DashboardCard({ name, id, htmlFile }: DashboardCardProps) {
   const router = useRouter();
 
   return (
     <Card
-      className="h-52 max-w-md cursor-pointer select-none overflow-hidden"
+      className="h-60 max-w-md cursor-pointer select-none overflow-hidden shadow"
       onClick={() => router.push(`/dashboard/${id}`)}
     >
       <CardHeader>
         <CardTitle>{name}</CardTitle>
       </CardHeader>
       <CardContent>
-        {/* <Image src={`/api/card-image/?id=${id}`} alt="Vercel" width={232} height={200} /> */}
-        {/* {imageUrl && <Image src={imageUrl} alt="Vercel" width={232} height={200} />} */}
+        <div className="flex items-center justify-center h-40">
+          <div style={{scale: "0.2", border: "1px solid black"}}>
+            {parse(htmlFile)}
+          </div>
+        </div>
       </CardContent>
     </Card>
   );
